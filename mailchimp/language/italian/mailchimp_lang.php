8597c450f3d32155ce2ff56abbb66c07a54cb60d
Pablo Benitez <pablos.benitez@gmail.com>
2013-06-04 13:30:26 -0300

    Adding Italian language file, thanks ChristianGiupponi

---
+++ mailchimp/language/italian/mailchimp_lang.php
@@ -0,0 +1,47 @@
+<?php
+// Error notifications
+$lang['mailchimp.no_api_key_error']		= 'Non è stata trovata nessuna API KEY, per favore inseriscila nelle Impostazioni alla voce MailChimp.';
+
+// Labels
+$lang['mailchimp.list_members_count_label']	     = 'Totale iscritti';
+$lang['mailchimp.list_click_rt_label']		     = 'Click Rate';
+$lang['mailchimp.list_default_from_name_label']	 = 'Def. From Name';
+$lang['mailchimp.list_default_from_email_label'] = 'Def. From Email';
+$lang['mailchimp.list_subscribers']              = 'Lista iscritti';
+$lang['mailchimp.no_records']                     = 'Non sono stati trovati record';
+
+$lang['mailchimp.updated_label']			= 'Ultimo aggiornamento';
+$lang['mailchimp.actions_label']			= 'Azioni';
+$lang['mailchimp.view_label']				= 'Vedi';
+$lang['mailchimp.manage_label']				= 'Gestisci';
+$lang['mailchimp.delete_label']				= 'Cancella';
+$lang['mailchimp.comments_enabled_label']	= 'Si';
+$lang['mailchimp.comments_disabled_label']	= 'No';
+$lang['mailchimp.published_yes_label']		= 'Si';
+$lang['mailchimp.published_no_label']		= 'No';
+$lang['mailchimp.subscriber_info']          = 'Dettagli';
+
+// General labels
+$lang['mailchimp.unknown_list_label']       = 'Lista Sconosciuta';
+$lang['mailchimp.first_name_label']       = 'Nome';
+$lang['mailchimp.last_name_label']       = 'Cognome';
+$lang['mailchimp.email_on_list']            = '%s su "%s"';
+$lang['mailchimp.all_subscribers_on_tlist'] = 'Tutti gli iscritti a questa lista';
+$lang['mailchimp.email_label']              = 'Email';
+$lang['mailchimp.new_memberinfo_label']     = 'Informazioni iscritto';
+$lang['mailchimp.last_changed_label']	    = 'Ultima modifica';
+$lang['mailchimp.new_suscriber_label']      = 'Aggiungi Iscritto';
+$lang['mailchimp.list_lists_label']		    = 'Elenco Liste';
+$lang['mailchimp.list_form_label']          = 'ID Lista';
+$lang['mailchimp.list_subscribers_label']   = 'Lista iscritti';
+$lang['mailchimp.list_subscribers_for_list_label']   = 'Elanco degli iscritti per la lista "%s"';
+$lang['mailchimp.id_label']			        = 'ID';
+$lang['mailchimp.web_id_label']			    = 'Web ID';
+$lang['mailchimp.status_label']			    = 'Stato';
+$lang['mailchimp.email_type_label']			= 'Tipo di email';
+$lang['mailchimp.ip_signup_label']          = 'Ip Signup';
+$lang['mailchimp.rating_label']			    = 'Voto';
+$lang['mailchimp.name_label']			    = 'Nome';
+
+// Frontend labels
+$lang['mailchimp.signup']		            = 'Unisciti';
